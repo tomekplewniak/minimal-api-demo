97566af1477a85f5037aa8f1d199636a4f85ab09
Tomek Plewniak <tomekplewniak@gmail.com>
2021-12-25 22:19:53 +0100

    Implement interaction with database.

---
+++ src/MinimalAPIDemoApp/DataAccess/Data/UserData.cs
@@ -0,0 +1,34 @@
+﻿using DataAccess.DbAccess;
+using DataAccess.Models;
+
+namespace DataAccess.Data;
+
+public class UserData : IUserData
+{
+    private readonly ISqlDataAccess _db;
+
+    public UserData(ISqlDataAccess db)
+    {
+        _db = db;
+    }
+
+    public async Task<IEnumerable<UserModel>> GetUsers() =>
+        await _db.LoadData<UserModel, dynamic>("dbo.spUser_GetAll", new { });
+
+    public async Task<UserModel?> GetUser(int id)
+    {
+        var results = await _db.LoadData<UserModel, dynamic>("dbo.spUser_Get",
+            new { Id = id });
+
+        return results.FirstOrDefault();
+    }
+
+    public async Task InsertUser(UserModel user) =>
+        await _db.SaveData("dbo.spUser_Insert", new { user.FirstName, user.LastName });
+
+    public async Task UpdateUser(UserModel user) =>
+        await _db.SaveData("dbo.spUser_Update", user);
+
+    public async Task DeleteUser(int id) =>
+        await _db.SaveData("dbo.spUser_Delete", new { Id = id });
+}
